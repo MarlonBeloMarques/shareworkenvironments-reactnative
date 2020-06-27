8a1ccd2b2adaa1ca491470395e8fb4ac3907fef3
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-06-27 09:52:19 -0400

    populating data

--- src/utils/data.js
+++ src/utils/data.js
@@ -76,6 +76,80 @@ const content = [
     image3:
       'https://revistanews.com.br/wp-content/uploads/2017/12/Um-dos-escrit%C3%B3rios-do-Google-1.jpg',
   },
+  {
+    id: '5',
+    like: false,
+    title: 'Google',
+    address: '2118 Thornridge Cir. Syracuse, Connecticut 35624',
+    about:
+      'Google LLC is a multinational software and services company in the United States. Google hosts and develops a range of internet-based products and services and generates profit primarily through advertising through AdWords. Google is the main subsidiary of Alphabet Inc.',
+    background:
+      'https://www.mundorh.com.br/wp-content/uploads/2017/12/ambiente-google.jpg',
+    backgroundWidth: 550,
+    backgroundHeight: 366,
+    image1:
+      'https://revistanews.com.br/wp-content/uploads/2017/12/Um-dos-escrit%C3%B3rios-do-Google-2.jpg',
+    image2:
+      'https://s32907.pcdn.co/wp-content/uploads/2019/08/area-decanso-escritorio-google.jpg',
+    image3:
+      'https://revistanews.com.br/wp-content/uploads/2017/12/Um-dos-escrit%C3%B3rios-do-Google-1.jpg',
+  },
+  {
+    id: '6',
+    like: true,
+    title: 'Facebook',
+    address: '8502 Preston Rd. Inglewood, Maine 98380',
+    about:
+      'Facebook is a social media and virtual social network launched on February 4, 2004, operated and privately owned by Facebook Inc .. On October 4, 2012, Facebook reached the mark of 1 billion active users, which is why largest virtual social network in the world.',
+    background:
+      'https://www.consueloblog.com/wp-content/uploads/2014/05/DSC04811.jpg',
+    backgroundWidth: 1280,
+    backgroundHeight: 852,
+    image1:
+      'https://i.pinimg.com/originals/00/62/33/00623309ef6201f4407f4dcbee45212e.jpg',
+    image2: 'https://dretrivelli.files.wordpress.com/2010/12/facebook1.jpg',
+    image3:
+      'https://i.pinimg.com/originals/05/e1/1c/05e11c16abcd279f3ceb8c0ff83c13b2.jpg',
+  },
+  {
+    id: '7',
+    like: false,
+    title: 'Nubank',
+    address: '1901 Thornridge Cir. Shiloh, Hawaii 81063',
+    about:
+      'Nubank is a pioneer Brazilian startup in the financial services segment, acting as a credit card and fintech operator with operations in Brazil, headquartered in São Paulo and founded on May 6, 2013 by David Vélez.',
+    background:
+      'https://i1.wp.com/blog.nubank.com.br/wp-content/uploads/2019/03/MG_6633.jpg?resize=1024%2C683&ssl=1',
+
+    backgroundWidth: 1024,
+    backgroundHeight: 683,
+    image1:
+      'https://blog.nubank.com.br/wp-content/uploads/2019/03/tourheader.png',
+    image2:
+      'https://i.pinimg.com/originals/00/b1/f5/00b1f5d11de671825b421ebde3b8d8ea.jpg',
+    image3:
+      'https://i.pinimg.com/originals/4c/0f/29/4c0f29125f0606b97973df84d4368fd0.jpg',
+  },
+  {
+    id: '8',
+    like: true,
+    title: 'Kojima Productions',
+    address: '2972 Westheimer Rd. Santa Ana, Illinois 85486',
+    about:
+      'Kojima Productions Co., Ltd. is a Japanese video game development studio, founded by Hideo Kojima, creator of the popular Metal Gear series. The company was created as a subsidiary of Konami.',
+    background:
+      'https://i1.wp.com/www.portallos.com.br/wp-content/uploads/2017/01/Kojima-Productions-Studio-004.jpg?fit=600%2C400&ssl=1',
+
+    backgroundWidth: 600,
+    backgroundHeight: 400,
+    image1:
+      'https://i0.wp.com/www.portallos.com.br/wp-content/uploads/2017/01/Kojima-Productions-Studio-001.jpg?fit=1200%2C800&ssl=1',
+
+    image2:
+      'https://i1.wp.com/www.portallos.com.br/wp-content/uploads/2017/01/Kojima-Productions-Studio-003.jpg?fit=600%2C400&ssl=1',
+    image3:
+      'https://i2.wp.com/www.portallos.com.br/wp-content/uploads/2017/01/Kojima-Productions-Studio-008.jpg?ssl=1',
+  },
 ];
 
 export { user, content };
